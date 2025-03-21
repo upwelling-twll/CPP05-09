5230f19ccc2d29ddde3baeef4c21c24d689a0b88
Natalia Magdanova <nmagdano@lab3r2s3.42abudhabi.ae>
2025-03-21 19:45:45 +0400

    CPP06:ex01: serializer and deserializer added

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -3,15 +3,16 @@
 
 int main()
 {
-	Data*		ptr = NULL;
+	Data		ptr;
 	uintptr_t	data;
-	Data*		againPtr;
+	Data*	againPtr;
 
-	ptr->horseName = "Spirit";
-	ptr->birthDate = 20200101;
-	ptr->breed = "Arabian";
-
-	data = Serializer::serialize(ptr);
+	ptr.horseName = "Spirit";
+	ptr.birthDate = 20200101;
+	ptr.breed = "Arabian";
+	data = Serializer::serialize(&ptr);
 	againPtr = Serializer::deserialize(data);
 	std::cout << againPtr->horseName << std::endl;
+	std::cout << againPtr->birthDate << std::endl;
+	std::cout << againPtr->breed << std::endl;
 }
